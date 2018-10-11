05918941502191d46af3245ee976ef7a08f873b2
Kevin Rosales <kevinrosales@Kevins-MacBook-Air.local>
2018-10-10 22:50:50 -0400

    just saving what i have right its a working progress

---
+++ main.js
@@ -0,0 +1,24 @@
+// const alphabetButtons = document.getElementById('buttons')
+// const images = document.getElementById('images')
+// const inputHold = document.getElementById('inputHold')
+// const categoryName = document.getElementById('categoryName')
+
+// const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+// const answerArray = []
+// const words = ['html', 'javascript', 'cascading style sheet', 'mario', 'luigi', 'browser']
+
+// function buttonAlpha() {
+//     for (let i = 0; i < alphabet.length; i++) {
+//         letters = 'alphabet'
+//         button1 = document.createElement('button')
+        
+
+
+//     }
+// }
+// const input = document.getElementsByClassName('text')
+// const button = document.getElementsByClassName('button')
+//     button.addEventListener('click', function(){
+//     alert('ssads')
+// })
+
