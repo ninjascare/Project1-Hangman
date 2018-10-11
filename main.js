026c13d2e2f8e04146a5adb64a335939872fe510
Kevin Rosales <kevinrosales@Kevins-MacBook-Air.local>
2018-10-11 15:15:06 -0400

    trying a possible eligible funtion for event listener

--- main.js
+++ main.js
@@ -1,24 +1,62 @@
-// const alphabetButtons = document.getElementById('buttons')
-// const images = document.getElementById('images')
-// const inputHold = document.getElementById('inputHold')
-// const categoryName = document.getElementById('categoryName')
 
-// const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-// const answerArray = []
-// const words = ['html', 'javascript', 'cascading style sheet', 'mario', 'luigi', 'browser']
+const inputHold = document.getElementById("inputHold");
+const catagoryName = document.getElementById("catagoryName");
+const playerLives  = document.getElementById('playerLives')
+const options = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z"
+];
+const words = [
+  "html",
+  "javascript",
+  "cascading style sheet",
+  "mario",
+  "luigi",
+  "browser"
+];
 
-// function buttonAlpha() {
-//     for (let i = 0; i < alphabet.length; i++) {
-//         letters = 'alphabet'
-//         button1 = document.createElement('button')
-        
+var optionChoices = function(){
+     alphabetButtons = document.getElementById("buttons")
+    alphaList = document.createElement('ul')
+    for (let i = 0; i < alphabet.length; i++){
+        alphaList.setAttribute('id', 'alphabet')
+        list = document.createElement('li')
+        //We are adding the <li> to the dom
+        list.setAttribute('id', 'letter')
+// were adding an id to the list items.
+        list.innerHTML = options[i]
+        //now im adding the alphabet letters to the list items
+        list.addEventListener('click', buttonClick)
 
+    }
+}
 
-//     }
-// }
-// const input = document.getElementsByClassName('text')
-// const button = document.getElementsByClassName('button')
-//     button.addEventListener('click', function(){
-//     alert('ssads')
-// })
+let buttonClick = function(evt){
+    evt.preventDefault()
+    
 
+}
