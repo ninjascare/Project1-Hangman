90284346fbb582a51f910d18759eb5f916a1499b
Kevin Rosales <kevinrosales@Kevins-MacBook-Air.local>
2018-10-11 18:06:02 -0400

    i am attemptting to create a fucntion that will respond to the click of the buttons

--- main.js
+++ main.js
@@ -29,23 +29,16 @@ const options = [
   "y",
   "z"
 ];
-const words = [
-  "yoshi",
-  "toad",
-  "princess peach",
-  "mario",
-  "luigi",
-  "browser"
-];
+const words = ["yoshi", "toad", "princess peach", "mario", "luigi", "browser"];
 
 var optionChoices = function() {
   alphabetButtons = document.getElementById("buttons");
   alphaList = document.createElement("ul");
   alphaList.setAttribute("id", "alphabet");
   for (let i = 0; i < options.length; i++) {
-    var list = document.createElement("li");
-    //We are adding the <li> to the dom
-    list.setAttribute("id", 'abc');
+    var list = document.createElement("button");
+    //We are adding the <button> to the dom
+    list.setAttribute("id", "abc");
     // were adding an id to the list items.
     list.innerHTML = options[i];
     //now im adding the alphabet letters to the list items
@@ -53,10 +46,30 @@ var optionChoices = function() {
   }
   alphabetButtons.appendChild(alphaList);
 };
-optionChoices()
+optionChoices();
 
+check = function(){
+    list.onclick = function(){
+        var attempt = this.innerHTML
+        this.setAttribute('class', 'active')
+        this.onclick = 0
+        for(let i = 0; i < words.length; i++) {
+            if(words[i]  === attempt){
+                guesses
+            }
+        }
 
+    }
+}
+var word = words[Math.floor(Math.random() * words.length)];
 
+function guess() {
+  let answer = [];
+  for (var i = 0; i < words.length; i++) {
+    answer[i] = "_";
+    inputHold.innerHTML
+  }
+  return answer
+}
 
-
-
+  var remainingLetters = words.length;
